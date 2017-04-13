74c0702acc3bcdfc2a5bb6c9957b16dacf1b2bcd
LauZi <st61112@gmail.com>
2017-04-14 04:39:10 +0800

    Initial Commit

---
+++ song/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  song
+//
+//  Created by Knife on 2017/4/14.
+//  Copyright © 2017年 Knife. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ song/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  song
+//
+//  Created by Knife on 2017/4/14.
+//  Copyright © 2017年 Knife. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
